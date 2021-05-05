f8c5bc0c52824cf0252e92156a29b3f9157dfc08
Mikhail Makarov <makarovmk77@gmail.com>
2021-05-05 10:25:59 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,84 @@ namespace Homework_06
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Метод для получения количества групп чисел
+        /// </summary>
+        /// <param name="amountOfNumbers">количество чисел в группе</param>
+        static void GetNumbersOfGroups(int amountOfNumbers)
+        {
+            int M;
+            M = (int)Math.Log(amountOfNumbers, 2) + 1;
+            Console.WriteLine($"Количество групп чисел: {M}");
+        }
+        /// <summary>
+        /// Метод для разбивки чисел на группы не делящихся друг для друга чисел, и записи этих групп в файл
+        /// </summary>
+        /// <param name="amountOfNumbers">количество чисел в группе</param>
+        static void GetGroups(int amountOfNumbers)
+        {
+            string tempString = ""; //врЕменная строка, нужна для дальнейшей записи
+            //int tempNumber = 2;     //временное число, необходимое для дальнейших расчётов, т.к. каждая группа начинается со степени двойки
+
+            ////File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", "1");
+            //Console.WriteLine("1"); //вывод единицы
+
+            //while (tempNumber * 2 < amountOfNumbers)                //пока степень двойки меньше исходного числа
+            //{
+            //    for (int i = tempNumber; i < tempNumber * 2; i++)   //добавляем числа в строку, начиная с одной степени двойки до следующей
+            //    {
+            //        tempString += $"{i} ";
+            //    }
+            //    //File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", $"\n{tempString}");
+            //    Console.WriteLine(tempString);                      //вывод строки с группой чисел                            
+            //    tempString = "";                                    //и обнуление строки
+            //    tempNumber *= 2;                                    //вычисляем следующую степень двойки
+            //}
+
+            ////tempNumber /= 2;   //в предыдущем цикле записывались в строки только числа до последней степени двойки. Чтобы вывести числа 
+            //                   //от последней степени двойки до последнего числа в ряду, возвращаем tempNumber к предыдущему значению
+
+            //for (int i = tempNumber; i <= amountOfNumbers; i++)     //выводим оставшиеся в ряду числа
+            //{
+            //    tempString += $"{i} ";
+            //}
+
+            ////File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", $"\n{tempString}");
+            //Console.WriteLine(tempString);
+            //string textGroups;   //задаём переменную для дальнейшей записи строки
+            //File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", textGroups); //создание файла/добавление строки в него
+
+
+            int count = 0;
+            Console.Write("1");
+            for (int i = 2; i <= amountOfNumbers; i++)
+            {
+                if (i % 2 == 0 && Math.Log(i, 2) % 1 == 0)
+                {
+                    if (Math.Log(i, 2) % 1 == 0)
+                    {
+                        Console.WriteLine("\n");
+                        Console.Write(i + " ");
+                        //File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", i + " ");
+                    }
+                    else
+                    {
+                        Console.Write(i + " ");
+                        //File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", i + " ");
+                    }
+                    //count += 1;
+                }
+
+                else
+                {
+                    //Console.Write(i + " ");
+                    //File.AppendAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\groups.txt", i + " ");
+                    //count += 1;
+                }
+            }
+        }
+             
+        static void Main()
         {
 
             /// Домашнее задание
@@ -71,6 +149,79 @@ namespace Homework_06
             ///   Как пометками, так и xml документацией
             ///   В обязательном порядке создать несколько собственных методов
 
+            Console.Clear();
+
+            string amountOfNumbersIn = "";
+            int amountOfNumbers = 0;
+
+            // проверка на тип данных, размер числа, диапазон от 1 до 1 000 000 000
+            do   
+            {
+                string fromFile = File.ReadAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\amount of numbers.txt");
+                int numFromFile;
+                bool isNum = int.TryParse(fromFile, out numFromFile);       //проверка на тип данных
+
+                if (isNum & numFromFile <= 1000000000 & numFromFile >= 1)   //проверка на размер числа, диапазон от 1 до 1 000 000 000
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Файл содержит данные неподходящего типа. \nВведите целое число от 1 до 1 000 000 000, сохраните файл \nи нажмите любую кнопку для повторного считывания");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (true);
+
+            amountOfNumbersIn = File.ReadAllText(@"d:\OneDrive\GitHub\Skillbox-Homework-Module6\amount of numbers.txt");      //считывание количества чисел из файла
+            amountOfNumbers = Convert.ToInt32(amountOfNumbersIn);                                                             //преобразование в int  
+
+            Console.WriteLine($"В файле обнаружен ряд чисел от 1 до {amountOfNumbers}.\n\nВыберите действие:");
+            Console.WriteLine($"\nОтобразить в консоли только количество групп - нажмите 1\nЗаписать группы в файл - нажмите 2\nВыполнить оба действия - нажмите 3");
+
+            string choise = Console.ReadLine(); //выбор действия
+
+            do
+            {
+                if (choise == "1")
+                {
+                    DateTime date = DateTime.Now;                  //засекаем время начала выполнения процедуры
+                    GetNumbersOfGroups(amountOfNumbers);
+                    TimeSpan span = DateTime.Now.Subtract(date);   // вычитаем из текущего времени время начала процедуры
+
+                    Console.WriteLine($"\nПосчитано за {span.TotalMilliseconds} мс ({span.TotalSeconds} сек или {span.TotalMinutes} мин)");
+                    
+                    Console.WriteLine($"\n\nЕсли хотите выполнить другое действие, нажмите 'д'. Чтобы выйти, нажмите любую клавишу");
+
+                    string repeat = Console.ReadLine();
+                    if (repeat == "д")
+                    {
+                        Main();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    break;
+                }
+                else
+                    if (choise == "2")
+                    {
+                        break;
+                    }
+                else
+                    if (choise == "3")
+                    {
+                        break;
+                    }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Неправильный ввод.\nВыберите действие");
+                    Console.WriteLine($"Отобразить в консоли только количество групп - нажмите 1\nЗаписать группы в файл - нажмите 2\nВыполнить оба действия - нажмите 3");
+                    choise = Console.ReadLine();
+                }
+            } while (true);
         }
     }
 }
